dcfbd8ed500b1711274ab2743402a6eb62631481
kawaldhillon05 <kawaldhillon5@gmail.com>
2023-12-08 00:26:47 -0500

    first stage of the game complete

--- script.js
+++ script.js
@@ -2,17 +2,24 @@ const game = (function () {
     const main = document.querySelector(".main");
 
     function createPlayer(){
-        const playerName = "";
-        const mark = "";
 
-        const setprop = (P_name, P_mark) => { playerName = P_name; mark = P_mark; };
+        let playerName = "";
+        let mark = "";
 
-        return {playerName, mark, setprop};
+        const getName = () =>  playerName;
+        const getMark = () =>  mark;
+        const setprop = function(P_name, P_mark){
+            playerName = P_name;
+            mark = P_mark; 
+        };
+        return {setprop, getMark, getName};
     }
 
     const start = () => {
-         
-        let mark_selected = "";
+
+        const p1 = createPlayer();
+        const p2 = createPlayer(); 
+        let mark_selected1 = "", mark_selected2 = "";
 
         const form = document.createElement("form");
         const formHead = document.createElement("p");
@@ -38,7 +45,7 @@ const game = (function () {
         Xbtn.setAttribute("type", "button");
         Obtn.setAttribute("type", "button");
         formbtn.textContent = "START GAME !";
-        formbtn.setAttribute("type","submit");
+        formbtn.setAttribute("type","button");
         main.appendChild(form);
         form.appendChild(formHead);
         form.appendChild(player1In);
@@ -49,16 +56,23 @@ const game = (function () {
         formMark.appendChild(Obtn);
         form.appendChild(formbtn);
 
-        const player1 = createPlayer();
-        const player2  = createPlayer();
-
-        Xbtn.addEventListener("mousedown", () => {Xbtn.classList.add("border_active","selected"); mark_selected = "x";})
-        Obtn.addEventListener("mousedown", () => {Obtn.classList.add("border_active","selected"); mark_selected = "o";})
+        Xbtn.addEventListener("mousedown", () => {Xbtn.classList.add("border_active","selected"); mark_selected1 = "x";mark_selected2 = "o";})
+        Obtn.addEventListener("mousedown", () => {Obtn.classList.add("border_active","selected"); mark_selected1 = "o";mark_selected2 = "x"})
         Xbtn.addEventListener("mouseup", () => {Xbtn.classList.remove("border_active");Obtn.classList.remove("selected");})
         Obtn.addEventListener("mouseup", () => {Obtn.classList.remove("border_active");Xbtn.classList.remove("selected");})
 
-        
-
+        formbtn.addEventListener("mousedown",() => {formbtn.classList.add("border_active");});
+        formbtn.addEventListener("mouseup",(e) => {formbtn.classList.remove("border_active");});
+        formbtn.addEventListener("click", () => {  
+            p1.setprop(player1In.value,mark_selected1);
+            p2.setprop(player2In.value,mark_selected2);
+            if((player1In.value === "")||(player2In.value === "")||(mark_selected1 === "")){
+                formbtn.classList.add("shake");
+                setTimeout(() =>{formbtn.classList.remove("shake")},200);
+            } else {
+                createGrid(9);
+            }
+        });
     }
 
 
