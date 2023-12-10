292992e7f32f79ff6e0bae1059971f1a11d81dc8
kawaldhillon05 <kawaldhillon5@gmail.com>
2023-12-10 16:27:15 -0500

    completed js file with functional game and OOP.

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const game = (function () {
     const main = document.querySelector(".main");
+    const p1 = createPlayer();
+    const p2 = createPlayer(); 
 
     function createPlayer(){
 
@@ -15,10 +17,7 @@ const game = (function () {
         return {setprop, getMark, getName};
     }
 
-    const start = () => {
-
-        const p1 = createPlayer();
-        const p2 = createPlayer(); 
+    const setPlayer = () => {
         let mark_selected1 = "", mark_selected2 = "";
 
         const form = document.createElement("form");
@@ -56,13 +55,13 @@ const game = (function () {
         formMark.appendChild(Obtn);
         form.appendChild(formbtn);
 
-        Xbtn.addEventListener("mousedown", () => {Xbtn.classList.add("border_active","selected"); mark_selected1 = "x";mark_selected2 = "o";})
-        Obtn.addEventListener("mousedown", () => {Obtn.classList.add("border_active","selected"); mark_selected1 = "o";mark_selected2 = "x"})
+        Xbtn.addEventListener("mousedown", () => {Xbtn.classList.add("border_active","selected"); mark_selected1 = "X";mark_selected2 = "O";})
+        Obtn.addEventListener("mousedown", () => {Obtn.classList.add("border_active","selected"); mark_selected1 = "O";mark_selected2 = "X"})
         Xbtn.addEventListener("mouseup", () => {Xbtn.classList.remove("border_active");Obtn.classList.remove("selected");})
         Obtn.addEventListener("mouseup", () => {Obtn.classList.remove("border_active");Xbtn.classList.remove("selected");})
 
         formbtn.addEventListener("mousedown",() => {formbtn.classList.add("border_active");});
-        formbtn.addEventListener("mouseup",(e) => {formbtn.classList.remove("border_active");});
+        formbtn.addEventListener("mouseup",() => {formbtn.classList.remove("border_active");});
         formbtn.addEventListener("click", () => {  
             p1.setprop(player1In.value,mark_selected1);
             p2.setprop(player2In.value,mark_selected2);
@@ -70,25 +69,138 @@ const game = (function () {
                 formbtn.classList.add("shake");
                 setTimeout(() =>{formbtn.classList.remove("shake")},200);
             } else {
+                main.removeChild(form);
                 createGrid(9);
             }
         });
     }
 
-
-
     const createGrid = (n) => {
-        main.textContent = "";
+        const main_top = document.createElement("div");
+        const main_bottom = document.createElement("button");
         const gameBoard = document.createElement("div");
+        main_bottom.classList.add("btn");
+        main_top.classList.add("grid_top");
+        main_bottom.textContent = "RESTART";
+        main_top.textContent = `${p1.getName()} will Start game by placing ${p1.getMark()}`;
         gameBoard.classList.add("game_board");
+        main.appendChild(main_top);
         main.appendChild(gameBoard);
+        main.appendChild(main_bottom);
         for(i = 0; i<n; i++){
             const cell = document.createElement("div");
             cell.classList.add("cell");
             cell.setAttribute("index", `${i}`);
             gameBoard.appendChild(cell);
         }
+        main_bottom.addEventListener("mousedown",() => {main_bottom.classList.add("border_active");});
+        main_bottom.addEventListener("mouseup",() => {
+        main_bottom.classList.remove("border_active");});
+        startPlaying();
     }
+
+    const startPlaying = () => {
+        const cells = document.querySelectorAll(".cell");
+        const statusText = document.querySelector(".grid_top");
+        const restartBtn = document.querySelector(".btn");
+        const winConditions = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6]
+        ];
+        let options = ["", "", "", "", "", "", "", "", ""];
+        let currentPlayer = p1;
+        let running = false;
+
+        initializeGame();
+
+        function initializeGame(){
+            cells.forEach(cell => cell.addEventListener("click",  cellClicked));
+            restartBtn.addEventListener("click", restartGame);
+            statusText.textContent = `${p1.getName()} will Start game by placing ${p1.getMark()}`;
+            running = true;
+        }
+        function cellClicked(){
+            const cellIndex = this.getAttribute("index");
+
+            if(options[cellIndex] != "" || !running){
+                return;
+            }
+
+            updateCell(this, cellIndex);
+            checkWinner();
+        }
+        function updateCell(cell, index){
+            options[index] = currentPlayer.getMark();
+            cell.textContent = currentPlayer.getMark();
+        }
+        function changePlayer(){
+            currentPlayer = (currentPlayer.getMark() == p1.getMark()) ? p2 : p1;
+            statusText.textContent = `${currentPlayer.getName()}'s turn`;
+        }
+        function checkWinner(){
+            let roundWon = false;
+
+            for(let i = 0; i < winConditions.length; i++){
+                const condition = winConditions[i];
+                const cellA = options[condition[0]];
+                const cellB = options[condition[1]];
+                const cellC = options[condition[2]];
+
+                if(cellA == "" || cellB == "" || cellC == ""){
+                    continue;
+                }
+                if(cellA == cellB && cellB == cellC){
+                    roundWon = true;
+                    cells.forEach(
+                        (cell) => {
+                            let index = cell.getAttribute("index")
+                            if (index == condition[0] || index == condition[1] || index == condition[2]) {
+                                cell.classList.add("cell_highlight");
+                            }
+                        }
+                    );
+                    console.log(condition);
+                    break;
+                }
+            }
+
+            if(roundWon){
+                statusText.textContent = `${currentPlayer.getName()} wins!`;
+                statusText.classList.add("winner")
+                running = false;
+            }
+            else if(!options.includes("")){
+                statusText.textContent = `Draw!`;
+                running = false;
+            }
+            else{
+                changePlayer();
+            }
+        }
+        function restartGame(){
+            
+            currentPlayer = p1;
+            options = ["", "", "", "", "", "", "", "", ""];
+            statusText.textContent = `${currentPlayer.getName()}'s turn`;
+            statusText.classList.remove("winner");
+            cells.forEach(cell => {
+                cell.textContent = "";
+                cell.classList.remove("cell_highlight");
+            });
+            running = true;
+        }
+    } 
+    
+    const start = () => {
+        setPlayer();
+    }
+
     return {start};
 })();
 
